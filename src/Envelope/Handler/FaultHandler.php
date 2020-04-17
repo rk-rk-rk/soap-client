1b68dc0ffd22b700c1316ca0394172660be3581f
Tomasz Kusy <tkusy@biostat.com.pl>
2020-04-17 15:55:03 +0200

    FaultHandler

--- src/Envelope/Handler/FaultHandler.php
+++ src/Envelope/Handler/FaultHandler.php
@@ -5,6 +5,9 @@ namespace GoetasWebservices\SoapServices\SoapClient\Envelope\Handler;
 use JMS\Serializer\DeserializationContext;
 use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
+use JMS\Serializer\JsonDeserializationVisitor;
+use JMS\Serializer\JsonSerializationVisitor;
+use JMS\Serializer\SerializationContext;
 use JMS\Serializer\XmlDeserializationVisitor;
 
 class FaultHandler implements SubscribingHandlerInterface
@@ -17,6 +20,18 @@ class FaultHandler implements SubscribingHandlerInterface
                 'format' => 'xml',
                 'type' => 'GoetasWebservices\SoapServices\SoapClient\Envelope\SoapEnvelope12\Parts\Fault\FaultDetail',
                 'method' => 'deserializeFaultDetail'
+            ),
+            array(
+                'direction' => GraphNavigator::DIRECTION_DESERIALIZATION,
+                'format' => 'json',
+                'type' => 'GoetasWebservices\SoapServices\SoapClient\Envelope\SoapEnvelope12\Parts\Fault\FaultDetail',
+                'method' => 'deserializeFaultDetailJson'
+            ),
+            array(
+                'direction' => GraphNavigator::DIRECTION_SERIALIZATION,
+                'format' => 'json',
+                'type' => 'GoetasWebservices\SoapServices\SoapClient\Envelope\SoapEnvelope12\Parts\Fault\FaultDetail',
+                'method' => 'serializeFaultDetailJson'
             ),
         );
     }
@@ -34,7 +49,14 @@ class FaultHandler implements SubscribingHandlerInterface
         }
         return $xml;
     }
+
+    public function serializeFaultDetailJson(JsonSerializationVisitor $visitor, $data, array $type, SerializationContext $context)
+    {
+        return $data;
+    }
+
+    public function deserializeFaultDetailJson(JsonDeserializationVisitor $visitor, $data, array $type, DeserializationContext $context)
+    {
+        return $data;
+    }
 }
-
-
-
